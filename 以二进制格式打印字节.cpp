5f7c017acdf145ae710e2f4e4954d55497c99b13
Highlight Clark <highlightz@163.com>
2015-10-16 23:30:55 +0800

    Update 以二进制格式打印字节.cpp

--- 以二进制格式打印字节.cpp
+++ 以二进制格式打印字节.cpp
@@ -4,6 +4,7 @@ using namespace std;
 /*
 * 表达式(1 << i)在每一个相继位的位置产生一个1，例如：10000000,01000000等。
 * 如果这一位和变量val按位与的结果不是零，就表明val的这一位为1。
+* 一般来说，在使用字节进行工作时，并不希望用带符号数。
 */
 void printBinary( const unsigned char val )
 {
