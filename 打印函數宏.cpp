d293fe8315cbe5451b09473f76174d1f58622fb6
Highlight Clark <highlightz@163.com>
2015-10-16 22:47:27 +0800

    Create 打印函數宏.cpp

---
+++ 打印函數宏.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#define PRINT(STR, VAR) \
+cout << STR << " = " << VAR << endl;
+
+int main( )
+{
+    int j = 2;
+    int i = 1;
+    PRINT( "j", j + i );
+    return 0;
+}
