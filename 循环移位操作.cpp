8755ab08de33818c03dc32e593fee5f121a296a4
Highlight Clark <highlightz@163.com>
2015-10-16 23:40:56 +0800

    Update 循环移位操作.cpp

--- 循环移位操作.cpp
+++ 循环移位操作.cpp
@@ -1,2 +1,26 @@
 // 左旋
-unsigned 
+unsigned char rol( unsigned char val )
+{
+  int high_bit;
+  if ( val & 0x80 )
+  {
+    high_bit = 1;
+  }
+  else
+  {
+    high_bit = 0;
+  }
+  // 左移操作，最低位变为零
+  val <<= 1;
+  // 将最高位值送给最低位
+  val |= high_bit;
+  return val;
+}
+#include <iostream>
+using namespace std;
+int main( )
+{
+  unsigned char i = 0x80;
+  cout << rol( i ) << endl;
+  return 0;
+}
